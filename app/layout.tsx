7b9d3363d1c16931455391101e3549bf66464108
MattQ <scarcella.matteo@icloud.com>
2026-07-20 14:03:57 +0200

    feat: added microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,17 @@ export default function RootLayout({ children }) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href="/icon.ico" />
+        {/* Microsoft Clarity analytics */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xpbas038pb");`,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
